aa44d177c87fda5f65a390b1bf72faf68cb9c367
karen <kiziasuma@gmail.com>
2025-01-15 15:58:27 +0100

    update

---
+++ python-hello_world/2-print.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+def print_script():
+    print("Programming is like building a multilingual puzzle")
+    return "Programming is like building a multilingual puzzle"
+
+if __name__ == "__main__":
+    print_script()
