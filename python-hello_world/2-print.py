a1059ace6c63878df263ee0dd8a4d5001d14464b
karen <kiziasuma@gmail.com>
2025-01-16 11:27:09 +0100

    r

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,11 +1,5 @@
 #!/bin/python3
-
-def print_script():
-    print("\"Programming is like building a multilingual puzzle")
-
-if __name__ == "__main__":
-    print_script()
-
+print("\"Programming is like building a multilingual puzzle")
 
 
 
