cd17dc8d10baf5ae18332ce7d2ac088eb6e5d478
karen <kiziasuma@gmail.com>
2025-01-16 13:23:46 +0100

    update

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
-def print_number(number):
-    print("Float: {:.2f}".format(number))
-
-if __name__ == "__main__":
-    number = 3.14159
-    print_number(number)
+number = 3.14159
+print("Float: {:.2f}" .format(number))
