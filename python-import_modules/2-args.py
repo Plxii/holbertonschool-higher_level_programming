bf906979496f14045433152a25aa7db8a1a7cf27
karen <kiziasuma@gmail.com>
2025-01-17 21:42:34 +0100

    update

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,9 +3,11 @@ import sys
 
 if __name__ == "__main__":
     numarg = len(sys.argv) - 1
-    if (numarg >= 1):
-        print("{} argument: ".format(numarg))
+    if (numarg == 0):
+        print("0 arguments.")
+    elif (numarg == 1):
+        print("1 arguments.")
     else:
-        print("{} arguments.".format(numarg))
-    for i in range(numarg, len(sys.argv)):
-        print("{}: {}".format({sys.argv}))
+        print("{} arguments:".format(numarg))
+    for i in range(numarg):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
