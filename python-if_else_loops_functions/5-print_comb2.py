bf04588fe421876bafa1b620759b98a5814b05af
karen <kiziasuma@gmail.com>
2025-01-17 17:28:24 +0100

    update

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for number in range(0, 100):
-    if (number < 99):
-        print("{number:02d}".format(number), end=", ")
+    if (number == 99):
+        print("{}".format(number))
     else:
-        print(number)
+        print("{:02d}".format(number), end=", ")
