b9705ae03ee2c2c2b0acda356bdf5794a2df92a2
karen <kiziasuma@gmail.com>
2025-02-03 15:11:42 +0100

    update

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+    """A base class for geometry-related operations.
+    """
+    def area(self):
+        """Public instance method that raises an Exception.
+        This method is intended to be overridden in subclasses.
+        When called, it raises an Exception with the message
+        'area() is not implemented' to indicate that the method
+        has not been implemented in this base class.
+        @self: The current instance of BaseGeometry, allowing access
+        to its attributes and methods.
+        """
+        raise Exception('area() is not implemented')
